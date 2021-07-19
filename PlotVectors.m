235a39c03ed61e74ce934df92ca36b98c57bb616
Amir Vaxman <avaxman@users.noreply.github.com>
2021-07-19 15:47:15 +0200

    Delete PlotVectors.m

--- PlotVectors.m
+++
@@ -1,3 +0,0 @@
-function PlotVectors(source, target, color)
-quiver3(source(:,1), source(:,2), source(:,3), target(:,1)-source(:,1), target(:,2)-source(:,2), target(:,3)-source(:,3),0, 'LineWidth', 2,'Color', color);
-end
